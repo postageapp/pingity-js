049d8737e3e2e34387b936c9307c29d5f88dc315
Emily <emily@mcminn.ca>
2019-01-17 15:17:04 -0500

    status icons for ajax request/response.

--- pingity.js
+++ pingity.js
@@ -1,26 +1,36 @@
 
-const username = "xE5Vj2EK503jtXPOfAQYo7Vb"
-const password = "TepNhXkENEcrbpHBro54EzvffVkByHTDkiQF9loPCOyfkcICvfvCnHIPhRCgtRO2"
+const PINGITY_ID = "xE5Vj2EK503jtXPOfAQYo7Vb"
+const PINGITY_SECRET = "TepNhXkENEcrbpHBro54EzvffVkByHTDkiQF9loPCOyfkcICvfvCnHIPhRCgtRO2"
 
 function ValidateWithPingity() {
   let input = document.getElementById('pingity-address').value
   console.log(input)
+
+  $(document).ajaxStart(function() {
+    $( "#success" ).hide();
+    $( "#warning" ).hide();
+    $( "#fail" ).hide();
+    $( "#working" ).show();
+  });
+
   var request = $.ajax({
     url: 'https://pingity.com/api/v1/reports',
     type: 'POST',
     data: { resource: input } ,
     beforeSend: function (xhr){ 
-        xhr.setRequestHeader('Authorization', 'Basic ' + btoa("xE5Vj2EK503jtXPOfAQYo7Vb" + ':' + "TepNhXkENEcrbpHBro54EzvffVkByHTDkiQF9loPCOyfkcICvfvCnHIPhRCgtRO2")); 
+        xhr.setRequestHeader('Authorization', 'Basic ' + btoa(PINGITY_ID + ':' + PINGITY_SECRET)); 
     },
     dataType: "json"
   });
 
-  request.done(function(data) {
+  request.done(function(report) {
     // your success code here
-    console.log(data);    
+    result = report[0].status.code;
+    return displayResult(result);    
   });
 
   request.fail(function(jqXHR, textStatus) {
+    $( "#oops" ).show();
       return ValidateWithRegex(input);
   });
 }
@@ -34,3 +44,28 @@ function ValidateWithRegex(input) {
     alert("You have entered an invalid email address!");
   }
 }
+
+function displayResult(result) {
+  switch (result) {
+    case "pass":
+      $( "#working" ).hide();
+      $( "#warning" ).hide();
+      $( "#fail" ).hide();
+      $( "#success" ).show();
+      break;
+    case "fail_critical":
+      $( "#working" ).hide();
+      $( "#success" ).hide();
+      $( "#warning" ).hide();
+      $( "#fail" ).show();
+      break;
+    case "warning":
+      $( "#working" ).hide();
+      $( "#success" ).hide();
+      $( "#fail" ).hide();
+      $( "#warning" ).show();
+      break;
+  }
+}
+
+
